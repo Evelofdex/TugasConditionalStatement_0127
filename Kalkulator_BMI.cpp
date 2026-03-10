d333a5b860b63af7b817af19a514959c5f3c6608
Evelofdex <evelofdex@gmail.com>
2026-03-10 20:25:55 +0700

    pembuatan prosedur untukl input

--- Kalkulator_BMI.cpp
+++ Kalkulator_BMI.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 float berat_badan, tinggi_badan;
 
+//bagian awal
+void warga_nanya(){
+    cout << "Masukkan berat badan: ";
+    cin >> berat_badan;
+    cout << "masukan tinggi badan (cm): ";
+    cin >> tinggi_badan;
+    tinggi_badan = tinggi_badan / 100;
+}
+
 int main (){
     
 }
